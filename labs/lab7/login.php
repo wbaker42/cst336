2238c7060ab8c146b8525c8a1696b370f9c3a5e1
William Baker <wibaker@csumb.edu>
2018-11-21 07:18:16 +0000

    adding lab7

--- labs/lab7/login.php
+++ labs/lab7/login.php
@@ -7,6 +7,7 @@
     <head>
         <title> Ottermart Login </title>
         <link href="css/styles.css" rel="stylesheet" type='text/css'/>
+        <link href= "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
         <h1> OtterMart Login </h1>
