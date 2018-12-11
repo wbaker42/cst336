25ac798fc7c81b17417c3a3f219dcd8dbefcf221
William Baker <wibaker@csumb.edu>
2018-12-11 05:20:08 +0000

    Final Project

--- finalProject/dbconnection.php
+++ finalProject/dbconnection.php
@@ -7,10 +7,11 @@
         if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
             $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
             $host = $url["host"];
-            $dbName = substr($url["path"], 1);
+            $dbname = substr($url["path"], 1);
             $username = $url["user"];
             $password = $url["pass"];
-        }
+        } 
+
         $dbConn= new PDO("mysql:host=$host; dbname=$dbname", $username, $password);
         
         $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
